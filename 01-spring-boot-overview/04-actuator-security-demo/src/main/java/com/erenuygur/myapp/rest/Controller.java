173ee709fe52795a6d8736aa00cea82c0d1e8e46
Eren <ernuygur@gmail.com>
2023-11-01 01:42:24 +0300

    add 1.4 spring actuator securiy demo project

---
+++ 01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/erenuygur/myapp/rest/Controller.java
@@ -0,0 +1,23 @@
+package com.erenuygur.myapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your luck day.";
+    }
+}
