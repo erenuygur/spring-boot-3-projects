201d9d0f3373e2261d71069348f90b2e57cdd2a3
Eren <ernuygur@gmail.com>
2023-11-13 02:09:31 +0300

    add 2.8 bean lifecycle methods

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/erenuygur/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,18 @@
+package com.erenuygur.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+    public BaseballCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+
+    @Override
+
+    public String getDailyWorkout() {
+        return "Spend 30 min in batting practice";
+    }
+}
