1074e4bff40e6ed22fe74deeb75d56b56e16c4c0
Eren <ernuygur@gmail.com>
2023-12-12 02:49:54 +0300

    add 3.4 student update

---
+++ 03-spring-boot-hibernate-jpa-crud/04-cruddemo-student-update/src/main/java/com/erenuygur/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,17 @@
+package com.erenuygur.cruddemo.dao;
+
+import com.erenuygur.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
+
+    void update(Student theStudent);
+}
