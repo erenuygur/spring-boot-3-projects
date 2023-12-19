c8bd9613e9e1c643a55e5aca1bbe6f7bc70c5c52
Eren <ernuygur@gmail.com>
2023-12-20 02:03:06 +0300

    add 3.5 delete students

---
+++ 03-spring-boot-hibernate-jpa-crud/05-cruddemo-student-delete/src/main/java/com/erenuygur/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,21 @@
+package com.erenuygur.cruddemo.dao;
+
+import com.erenuygur.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer studentId);
+
+    int deleteAll();
+}
